00c8d2e8f72461d18191e78e3c0ea050a9226e0f
Mufasa <fashioncstar2019@outlook.com>
2019-09-22 19:29:58 +0700

    run
     run

---
+++ app/src/main/java/rk/team/file_upload/ImageActivity.java
@@ -0,0 +1,77 @@
+/**----------------------------------------------------------------------------------
+* Microsoft Developer & Platform Evangelism
+*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+* THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+* OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+*----------------------------------------------------------------------------------
+* The example companies, organizations, products, domain names,	
+* e-mail addresses, logos, people, places, and events depicted
+* herein are fictitious.  No association with any real company,
+* organization, product, domain name, email address, logo, person,
+* places, or events is intended or should be inferred.
+*----------------------------------------------------------------------------------
+**/
+
+package rk.team.file_upload;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        final ImageView imageView = (ImageView)findViewById(R.id.ChildImageView);
+
+        final String image = this.getIntent().getStringExtra("image");
+
+        final ByteArrayOutputStream imageStream = new ByteArrayOutputStream();
+
+        final Handler handler = new Handler();
+
+        Thread th = new Thread(new Runnable() {
+            public void run() {
+
+                try {
+
+                    long imageLength = 0;
+
+                    ImageManager.GetImage(image, imageStream, imageLength);
+
+                    handler.post(new Runnable() {
+
+                        public void run() {
+                            byte[] buffer = imageStream.toByteArray();
+
+                            Bitmap bitmap = BitmapFactory.decodeByteArray(buffer, 0, buffer.length);
+
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    });
+                }
+                catch(Exception ex) {
+                    final String exceptionMessage = ex.getMessage();
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Toast.makeText(ImageActivity.this, exceptionMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }});
+        th.start();
+    }
+}
